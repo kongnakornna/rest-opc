caaaa27c8750a3573040dfddbaf31f7763b28236
Michiel Meertens <michielmeertens@MacBook-Pro-van-Michiel.local>
2016-06-27 12:50:04 +0200

    Initial commit

---
+++ src/OpenCartRestClient.php
@@ -0,0 +1,60 @@
+<?php namespace MCS;
+
+use Exception;
+
+class OpenCartRestClient{
+
+    private $apiUser;
+    private $apiPass;
+    private $apiUrl;
+    private $cookie = null;
+    
+    public function __construct($user, $pass, $url)
+    {
+        $this->apiUser = $user;
+        $this->apiPass = $pass;
+        $this->apiUrl = $url . 'index.php?';
+    }
+    
+    private function request($query, $post = null)
+    {
+        
+        $array = [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $this->apiUrl . http_build_query($query),
+            CURLOPT_POSTFIELDS => http_build_query([
+                'username' => $this->apiUser,
+                'password' => $this->apiPass,
+                'key' => $this->apiPass
+            ])
+        ];
+        
+        $ch = curl_init();
+        curl_setopt_array($ch, $array);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        
+        return json_decode($result, true);
+        
+    }
+    
+    public function getorders()
+    {
+
+        $query = [
+            'action' => 'orders'
+        ];
+        return $this->request($query);
+    }
+    
+    public function getOrderItems($order_id)
+    {
+
+        $query = [
+            'id' => $order_id,
+            'action' => 'order_products'
+        ];
+        return $this->request($query);
+    }
+   
+}
